bb60bd96d14400f32d838b381f82d3a2fe6c1b45
Ranjith B K <ranjithkeerthivasan@gmail.com>
2024-12-28 23:27:31 +0530

    [RA]Project start

---
+++ Javascript/scripts/array.js
@@ -0,0 +1,7 @@
+var fruit = ["apple","orange","grape","banana"]
+// console.log(fruit)
+// console.log(fruit[0])
+console.log(fruit.length)
+for(count=0;count<fruit.length;count=count+1){
+    console.log(fruit[count])
+}
